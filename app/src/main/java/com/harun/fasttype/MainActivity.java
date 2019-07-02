b70fdaddcfb190cd0be093da72c773e9f6c5e051
Harun Oğuz <34753013+hrnoguz27@users.noreply.github.com>
2019-07-02 11:25:24 +0300

    Proje Oluşturuldu!

--- app/src/main/java/com/harun/fasttype/MainActivity.java
+++ app/src/main/java/com/harun/fasttype/MainActivity.java
@@ -52,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(
-                    new InputStreamReader(getAssets().open("data.text"), "UTF-8"));
-
+                    new InputStreamReader(getAssets().open("dataeng.txt"), "UTF-8"));
+            //Yazma hatası giderildi....
             // do reading, usually loop until end of file reading
             String mLine;
             while ((mLine = reader.readLine()) != null) {
